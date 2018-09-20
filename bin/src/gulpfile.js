aa862fd34a4dc312565263faedee15f31e02f219
Max <max@ehlers.berlin>
2018-09-20 13:57:29 +0200

    includes variables file for css

--- bin/src/gulpfile.js
+++ bin/src/gulpfile.js
@@ -1,85 +1,109 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 
 // gulp plugins and utils
-const gutil = require('gulp-util');
-const livereload = require('gulp-livereload');
-const postcss = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
-const zip = require('gulp-zip');
-const watch = require('gulp-watch');
+const gutil = require("gulp-util");
+const livereload = require("gulp-livereload");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const zip = require("gulp-zip");
+const watch = require("gulp-watch");
 
 // postcss plugins
-const autoprefixer = require('autoprefixer');
-const colorFunction = require('postcss-color-function');
-const cssnano = require('cssnano');
-const customProperties = require('postcss-custom-properties');
-const easyimport = require('postcss-easy-import');
+const autoprefixer = require("autoprefixer");
+const colorFunction = require("postcss-color-function");
+const cssnano = require("cssnano");
+const customProperties = require("postcss-custom-properties");
+const easyimport = require("postcss-easy-import");
 
 const swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
+  gutil.log(error.toString());
+  gutil.beep();
+  this.emit("end");
 };
 
-const nodemonServerInit = function () {
-    livereload.listen(1234);
+const nodemonServerInit = function() {
+  livereload.listen(1234);
 };
 
-const source = './',  
-  destination = './docker-mount';
+const source = ".",
+  destination = "./docker-mount";
 
-gulp.task('build', ['css'], function (/* cb */) {
+gulp.task("css", function() {
+  const processors = [
+    customProperties({
+      importFrom: `${source}/assets/css/helpers/variables.css`,
+      preserve: false
+    }),
+    easyimport,
+    colorFunction(),
+    autoprefixer({ browsers: ["last 2 versions"] }),
+    cssnano()
+  ];
+
+  return gulp
+    .src(`${source}/assets/css/*.css`)
+    .on("error", swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("assets/built/"))
+    .pipe(livereload());
+});
+
+gulp.task(
+  "build",
+  gulp.series("css", function(/* cb */) {
     return nodemonServerInit();
-});
+  })
+);
 
-gulp.task('css', function () {
-    const processors = [
-        easyimport,
-        customProperties,
-        colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
-        cssnano()
+gulp.task(
+  "watch",
+  gulp.series("css", function() {
+    const foldersToWatch = [
+      "**",
+      "!node_modules",
+      "!node_modules/**",
+      "!zip",
+      "!zip/**",
+      "!docker-mount",
+      "!docker-mount/**",
+      "!docker-compose.yml",
+      "!gulpfile.js"
     ];
-
-  return gulp.src(`${source}/assets/css/*.css`)
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
-});
-
-gulp.task('watch', function () {
-  const foldersToWatch = [
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!zip', '!zip/**',
-        '!docker-mount', '!docker-mount/**',
-        '!docker-compose.yml',
-        '!gulpfile.js'
-    ];
-    gulp.watch(`${source}/assets/css/**`, ['css']);
-    gulp.src(foldersToWatch, {base: source})
-      .pipe(watch(foldersToWatch, {base: source}))
+    gulp.watch(`${source}/assets/css/**`, gulp.parallel("css"));
+    gulp
+      .src(foldersToWatch, { base: source })
+      .pipe(watch(foldersToWatch, { base: source }))
       .pipe(gulp.dest(destination));
-});
+  })
+);
 
-gulp.task('zip', ['css'], function () {
-    const targetDir = 'zip/';
-    const themeName = require('./package.json').name;
-    const filename = themeName + '.zip';
+gulp.task(
+  "zip",
+  gulp.series("css", function() {
+    const targetDir = "zip/";
+    const themeName = require("./package.json").name;
+    const filename = themeName + ".zip";
 
-    return gulp.src([
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!zip', '!zip/**',
-        '!docker-mount', '!docker-mount/**'
-    ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
-});
+    return gulp
+      .src([
+        "**",
+        "!node_modules",
+        "!node_modules/**",
+        "!zip",
+        "!zip/**",
+        "!docker-mount",
+        "!docker-mount/**"
+      ])
+      .pipe(zip(filename))
+      .pipe(gulp.dest(targetDir));
+  })
+);
 
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
-});
+gulp.task(
+  "default",
+  gulp.series("build", function() {
+    gulp.start("watch");
+  })
+);
